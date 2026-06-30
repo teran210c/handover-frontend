be92566cefd4a7046d14b33e9408917dd8adc16b
teran210c <162738757+teran210c@users.noreply.github.com>
2026-06-30 23:13:38 +0000

    Feat: Added date and button to add defects

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,8 @@ import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 
 export default function Layout() {
+    const today = new Date()
+    console.log(today)
     return (
         <>
             <div className="min-h-screen flex">
@@ -9,7 +11,25 @@ export default function Layout() {
                 <div className="flex-1 flex flex-col">
                     <Navbar />
                     <main className="flex-1">
-                        content
+                        <h1>
+                            {today.toLocaleDateString('en-US', {
+                                weekday: 'short',
+                                month: 'short',
+                                day: 'numeric',
+                                year: 'numeric'
+                            })}
+                        </h1>
+
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Add Defect
+                        </button>
+
+                        <ol>
+                            <li>
+                                lista de defetcs
+                            </li>
+                        </ol>
+
                     </main>
                 </div>
             </div>
