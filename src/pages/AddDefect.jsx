f81e12514eac98dc43485f14796c97e93f447624
teran210c <162738757+teran210c@users.noreply.github.com>
2026-07-02 01:07:17 +0000

    Feat: set up routes and set up form for upload defect

--- src/pages/AddDefect.jsx
+++ src/pages/AddDefect.jsx
@@ -1,7 +1,98 @@
+import { useLocation } from "react-router-dom"
+import { useState } from "react";
+
+
 export default function AddDefect() {
-    return(
+    const location = useLocation()
+    const date = location.state.dateSent
+
+    const mockUser = {
+        id: "usr-992",
+        name: "Carlos Terán"
+    }
+
+    const [formData, setFormData] = useState({
+        date: location.state.dateSent,
+        reporterId: mockUser.id,
+        reporterName: mockUser.name,
+        description: ""
+    });
+
+
+    return (
         <div>
-            add
+            <form>
+                <div>
+                    <div>
+                        <label htmlFor="date-id">Registry Date</label>
+                        <input
+                            id="date-id"
+                            type="text"
+                            value={formData.date}
+                            readOnly
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor="line-id"
+
+                        >
+                            Line
+                        </label>
+                        <select name="" id="">
+                            <option value={''}>-- Select your line --</option>
+                            <option value={'L1'}>L1</option>
+                            <option value={'L2'}>L2</option>
+                            <option value={'L3'}>L3</option>
+                            <option value={'L5'}>L5</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="mo-id">MO</label>
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">ISN</label>
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Station</label>
+                        <select name="" id="">
+                            <option value="">-- Select station --</option>
+                            <option value="post-aoi">Post AOI</option>
+                            <option value="ict">ICT</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Model</label>
+                        <select name="" id="">
+                            <option value="bcm031">BCM031</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Shift</label>
+                        <select name="" id="">
+                            <option value="">-- Select shift</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Process side</label>
+                        <select name="" id="">-- Select side --</select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Location</label>
+                        <input type="text" />
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
--- src/pages/Defects.jsx
+++ src/pages/Defects.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom"
 
-export default function defects() {
-     const today = new Date()
+export default function Defects() {
+    const today = new Date()
+    const formattedDate = today.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    })
     return (
         <main className="flex-1">
-            <h1>
-                {today.toLocaleDateString('en-US', {
-                    weekday: 'short',
-                    month: 'short',
-                    day: 'numeric',
-                    year: 'numeric'
-                })}
-            </h1>
+            <h1>{formattedDate}</h1>
 
-            <Link to="/add-defects" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-             Add Defect
+            <Link
+                to="/add-defects"
+                state={{ dateSent: formattedDate }}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Add Defect
             </Link>
 
             <ol>
